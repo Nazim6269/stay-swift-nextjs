db32478a732d3c0d46d9fc0aa0ee0df9aa46c033
nazimuddin <nazimuddin102001@gmail.com>
2024-08-14 12:37:06 +0600

    implemented login with credentials using authjs

--- auth.js
+++ auth.js
@@ -1,5 +1,6 @@
 import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import {
@@ -10,11 +11,15 @@ import {
   googleSecret,
 } from './ secret';
 import mongoClientPromise from './lib/client';
+import { userModel } from './models/usersModels';
 
 const authOptions = {
   adapter: MongoDBAdapter(mongoClientPromise, {
     databaseName: environmentSecret,
   }),
+  session: {
+    strategy: 'jwt',
+  },
   providers: [
     GithubProvider({
       clientId: githubId,
@@ -24,6 +29,34 @@ const authOptions = {
     GoogleProvider({
       clientId: googleId,
       clientSecret: googleSecret,
+    }),
+
+    CredentialsProvider({
+      credentials: {
+        email: {},
+        password: {},
+      },
+
+      async authorize(credentials) {
+        if (!credentials) return null;
+
+        try {
+          const user = await userModel.findOne({ email: credentials.email });
+          if (user) {
+            const isMatchPass = user.password === credentials.password;
+
+            if (isMatchPass) {
+              return user;
+            } else {
+              throw new Error('Invalid User or Password');
+            }
+          } else {
+            throw new Error('Invalid User or Password');
+          }
+        } catch (error) {
+          throw new Error('user not found');
+        }
+      },
     }),
   ],
 };
--- components/auth/Login.jsx
+++ components/auth/Login.jsx
@@ -1,20 +1,55 @@
+'use client';
+
+import { login } from '@/actions';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+//TODO: not redirect bookings afte login with credentials
+
 const Login = () => {
+  const [error, setError] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const formData = new FormData(e.currentTarget);
+      const res = await login(formData);
+
+      if (!!res.error.message) {
+        setError(res.error.message);
+      } else {
+        router.push('/bookings');
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
   return (
-    <form className="login-form">
+    <>
       <div>
-        <label htmlFor="email">Email Address</label>
-        <input type="email" name="email" id="email" />
+        {error && (
+          <span className="text-red-600 text-center text-xl">
+            {error.message}
+          </span>
+        )}
       </div>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email Address</label>
+          <input type="email" name="email" id="email" />
+        </div>
 
-      <div>
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
-      </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" />
+        </div>
 
-      <button type="submit" className="btn-primary w-full mt-4">
-        Login
-      </button>
-    </form>
+        <button type="submit" className="btn-primary w-full mt-4">
+          Login
+        </button>
+      </form>
+    </>
   );
 };
 
