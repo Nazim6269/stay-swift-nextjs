da0c87b06529a56d9ffebc9209c8cdfb5b945455
nazimuddin <nazimuddin102001@gmail.com>
2024-08-15 10:39:31 +0600

    implemented user profile with actual data

--- components/users/ProfileInfo.jsx
+++ components/users/ProfileInfo.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import { auth } from '@/auth';
+import Image from 'next/image';
 
-const ProfileInfo = () => {
+const ProfileInfo = async () => {
+  const session = await auth();
+  console.log(session, 'auth');
   return (
     <section className="mt-[100px]">
       <div className="container">
         {/* <!-- profile info --> */}
         <div className="flex flex-col items-center py-8 text-center">
           {/* <!-- profile image --> */}
-          <div className="relative max-h-[180px] max-w-[180px] rounded-full lg:mb-8 h-[100px] w-[100px] bg-orange-600 grid place-items-center text-4xl text-white">
-            N
+          <div className="relative max-h-[180px] max-w-[180px] rounded-full lg:mb-8 h-[100px] w-[100px] grid place-items-center text-4xl text-white">
+            {session?.user?.image ? (
+              <Image
+                width={90}
+                height={32}
+                src={session?.user?.image}
+                className="rounded-full"
+                alt={session?.user?.name}
+              />
+            ) : (
+              <div className="max-h-[180px] max-w-[180px] rounded-full lg:mb-8 h-[100px] w-[100px] grid place-items-center text-4xl bg-gray-700 text-white">
+                {session?.user?.name.charAt(0).toUpperCase()}
+              </div>
+            )}
           </div>
           {/* <!-- name , email --> */}
           <div>
-            <h3 className="text-2xl font-semibold lg:text-[28px]">
-              nazimuddin
+            <h3 className="text-2xl bg-bl font-semibold lg:text-[28px]">
+              {session?.user?.name}
             </h3>
-            <p className="leading-[231%] lg:text-lg">nazim@gmail.com</p>
+            <p className="leading-[231%] lg:text-lg">{session?.user?.email}</p>
           </div>
 
           <div className="w-3/4 border-b border-[#a4a4a4] py-6 lg:py-4"></div>
